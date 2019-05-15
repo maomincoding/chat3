8763b9fa603afdd5cf600297df4721263bf2e531
maomincoding <815682884@qq.com>
2019-05-15 14:53:23 +0800

    init

---
+++ public/js/index.js
@@ -0,0 +1,260 @@
+    var socket = io.connect("106.13.131.245:3003");
+    var re = document.querySelector("#re");
+    var register1=document.querySelector(".register");
+    var init =document.querySelector(".init");
+    var passr=document.querySelector("#passr");
+    var passl=document.querySelector("#passl");
+    var login1= document.querySelector(".login");
+    var register_b = document.querySelector("#register_b");
+    var lo = document.querySelector("#lo");
+    var chat = document.querySelector("#chat");
+    var login_b = document.querySelector("#login_b");
+    var myMes = "";
+    var vf="";
+    var na = "";
+    var p = "";
+    var we = "";
+    var div = "";
+    var v="";
+    var q=0;
+    var regCn = /[@:]/im;
+    var pattern = /^[\u4E00-\u9FA5]{1,5}$/;
+// 初始页面注册
+    document.querySelector("#reg").onclick=function(){
+        register1.style.display="block";
+        init.style.display="none";
+        document.querySelector(".bg").style.display="none";
+    }
+// 初始页面登录
+    document.querySelector("#log").onclick=function(){
+        login1.style.display="block";
+        init.style.display="none";
+        document.querySelector(".bg").style.display="none";
+    }
+// 登录按钮
+    login_b.onclick = function () {
+        login();
+       
+    }
+// 注册按钮
+    register_b.onclick=function () {
+        register();
+    }
+//发送
+    document.getElementById("btn").onclick = function () {
+        send(); 
+    };
+// 内容填充
+    document.getElementById("text").onkeyup = function () {
+        if (document.getElementById("text").value.length != 0) {
+            document.getElementById("btn").style.cssText = "background:#98E165;color:#fff;"
+        } else {
+            document.getElementById("btn").style.cssText = "background: #DDDEE2;color:#fff"
+        }
+    }
+    document.querySelector("#text").onclick = function () {
+        document.querySelector('#text').scrollIntoView(false);
+    }
+// 传名
+ var users2 = "";
+ socket.on('users', function (users) {
+     users2 = users;
+    //  console.log(users2);
+ });
+ // 传密码
+ var pass2=""
+  socket.on('pass', function (val) {
+     pass2 = val;
+    //  console.log(pass2)
+ });
+// 统计在线人数
+  socket.on('dataval', function (val) {
+      document.querySelector(".fix").innerHTML = "";
+      vf = val;
+      console.log(vf);
+      for (let i = 0; i < vf.length; i++) {
+            var li = document.createElement("li");
+            li.innerText = vf[i];
+            console.log(vf[i]);
+            socket.emit("time", vf[i]);
+            document.querySelector(".fix").appendChild(li);
+      }
+     
+  });
+// 生成数组
+  var ar="";
+  socket.on('array', function (val) {
+    ar = val;
+    // console.log(ar);
+});
+// 在线信息
+  var onOff = true;
+document.querySelector(".title img").onclick=function (arams) {
+    onOff ? document.querySelector(".fix").style.display = "block" : document.querySelector(".fix").style.display = "none";
+    onOff = !onOff;
+}
+ // 封装注册
+function register() {
+    if (re.value.length == 0) {
+        sweetAlert("请输入用户名！");
+        return false;
+    } else if (regCn.test(re.value)) {
+        sweetAlert("格式错误，不能够用和:符号取名，请重新输入！");
+        return false;
+    } else if (pattern.test(re.value)) {
+        sweetAlert("不能使用中文字符哦！");
+        return false;
+    } else if (!(re.value.length == 0 && regCn.test(re.value))) {
+        if (users2.indexOf(re.value) != -1) {
+              sweetAlert("已经注册啦,换一个用户名吧！");
+        }
+        else{
+             names(re.value.trim());
+             pass(passr.value.trim());
+             sweetAlert("注册成功,您的用户名:" + re.value.trim());
+             document.querySelector(".swal-button").onclick = function () {
+                window.location.reload();
+             }
+        }
+    } 
+}
+// 封装登录
+    function login() {
+       if (lo.value.length == 0) {
+           sweetAlert("请输入用户名！");
+           return false;
+       } else if (regCn.test(lo.value)) {
+           sweetAlert("格式错误，不能够用和:符号取名，请重新输入！");
+           return false;
+       } else if (pattern.test(lo.value)) {
+           sweetAlert("不能使用中文字符哦！");
+           return false;
+       } else if (!(lo.value.length == 0 && regCn.test(lo.value))) {
+           if (users2.indexOf(lo.value) != -1) {
+            for (var i = 0; i < users2.length; i++) {
+                if(users2[i]===lo.value&&pass2[i]===passl.value){
+                    if(ar.indexOf(lo.value)==-1){
+                        sweetAlert("恭喜您，登录成功！");
+                        socket.emit('setName', lo.value.trim());
+                        names1(lo.value.trim());
+                        login1.style.display="none";
+                        document.querySelector(".bg").style.display = "none";
+                        document.querySelector(".cd span").style.display = "none";
+                        document.querySelector(".title img").style.display = "block";
+                        document.querySelector(".fix").style.display = "block" ;
+                        document.querySelector(".title").style.display = "block" ;
+                        document.querySelector(".tit").innerText=lo.value.trim();
+                        document.querySelector(".swal-button").onclick = function () {
+                        document.getElementById("text").focus();
+                        document.querySelector(".fix").addEventListener('click', function (e) {
+                           if (e.target.nodeName === "LI"&&e.target.innerText != document.querySelector(".tit").innerText) {
+                                document.querySelector(".fix").style.display="none";
+                                document.querySelector(".chat_b").style.display="block";
+                                document.querySelector(".box").style.display="block";
+                                document.querySelector(".tit").innerText = e.target.innerText;
+                                document.querySelector("#text").focus();
+                                onOff=true;
+                           }
+                           else{
+                            sweetAlert("不能跟自己聊天哦~");
+                           }
+                        })
+                    }
+                }
+                else{ sweetAlert("不能重复登录哦！"); return }
+            }
+            if(users2[i]===lo.value&&pass2[i]!=passl.value){ sweetAlert("密码错误！"); return; }
+            } 
+           } 
+           else { sweetAlert("请先注册哦！"); login1.style.display="none"; register1.style.display="block"; }
+       }                  
+    }
+// 传名
+    function names(value) {
+        this.name=value;
+        socket.emit("reg", name);
+    }
+    function names1(value) {
+        this.name1 = value;
+        socket.emit("join", name1);
+        document.title = name1 + "的臻美Chat"
+    }
+// 传密码
+    function pass(value){
+         socket.emit("pass", value);
+    }
+    socket.on("join", function (user) {
+        this.na = user; 
+    })
+    socket.on("reg", function (user) {
+        this.na1 = user;
+    })
+    socket.on("pass", function (val) {
+        // console.log(val);
+    })
+// 私发消息
+    socket.on('message1', function (data) {
+        var p1 = document.createElement("div");
+        var s1 = document.createElement("p");
+        var s2 = document.createElement("p");
+        var div1 = document.createElement("div");
+        var em=document.createElement("em");
+        var ads = document.createElement("audio");
+        ads.src = "http://106.13.131.245/data/s.wav";
+        ads.className = "ads";
+        s1.className="chatlist";
+        s2.className="chatlist1";
+        em.className="zwasked1";
+        div1.className = "divbox";
+        s1.innerText= data.from;
+        s2.innerText= data.msg;
+        s1.appendChild(em);
+        p1.appendChild(s1);
+        p1.appendChild(s2);
+        chat.appendChild(ads);
+        ads.play();
+        div1.appendChild(p1);
+        chat.appendChild(div1);
+        chat.scrollTop = chat.scrollHeight;
+    });
+// 私聊发送
+    function send() {
+            if (document.getElementById("text").value != "") {
+                 socket.emit('sayTo', {
+                     from: lo.value,
+                     to: document.querySelector(".tit").innerText,
+                     msg: document.querySelector("#text").value,
+                 })
+                var p1 = document.createElement("div");
+                 var s1 = document.createElement("p");
+                var s2 = document.createElement("p");
+                var em=document.createElement("em");
+                var div1 = document.createElement("div");
+                var ads = document.createElement("audio");
+                p1.style.cssText="float:right;";
+                s2.style.cssText = "color:#333;"
+                ads.src = "https://maomincoding.github.io/chatdata/s.wav";
+                ads.className = "ads";
+                div1.className = "divbox";
+                s1.className="chatlist";
+                s1.style.cssText="color:#333 !important;float:right; !important";
+                s2.className="chatlist2";
+                em.className="zwasked";
+                s1.innerText=lo.value;
+                s2.innerText= document.querySelector("#text").value;
+                s1.appendChild(em);
+                p1.appendChild(s1);
+                p1.appendChild(s2);
+                chat.appendChild(ads);
+                ads.play();
+                div1.appendChild(p1);
+                chat.appendChild(div1);
+                chat.scrollTop = chat.scrollHeight;
+  
+            } else {
+                sweetAlert('请输入内容！');
+            }
+         chat.scrollTop = chat.scrollHeight;
+         document.querySelector("#text").value = "";
+         document.querySelector("#text").focus();
+}
---
+++ server.js
@@ -0,0 +1,154 @@
+
+var http=require("http");
+var fs=require("fs");
+var express = require('express');
+var ws=require("socket.io");
+var path=require("path");
+var _ = require('underscore');
+var usocket = [];
+var usocket1 = [];
+var pass=[];
+var data=[];
+var hashName = {};
+var onlineCount = 0;
+var app = express();
+// 静态文件识别
+app.use(express.static(path.join(__dirname, './public')));
+var server=http.createServer(function (req,res) {
+    var filename = req.url.split('/')[req.url.split('/').length-1];
+    var suffix = req.url.split('.')[req.url.split('.').length-1];
+    if(req.url==='/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        var html = fs.readFileSync("./public/index.html");
+        res.end(html)
+    }else if(suffix==='css'){
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(get_file_content(path.join(__dirname, 'public', 'css', filename)));
+    }else if(suffix==='js') {
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.end(get_file_content(path.join(__dirname, 'public', 'js', filename)));
+    }else if (suffix in ['gif', 'jpeg', 'jpg', 'png']) {
+        res.writeHead(200, {
+            'Content-Type': 'image/' + suffix
+        });
+        res.end(get_file_content(path.join(__dirname, 'public', 'images', filename)));
+    }
+});
+function get_file_content(filepath) {
+    return fs.readFileSync(filepath);
+}
+// 获取在线
+function broadcast() {
+    io.sockets.emit("dataval", hashName);
+}
+//提供私有socket
+function privateSocket(toId) {
+    return (_.findWhere(io.sockets.sockets, {
+        id: toId
+    }));
+}
+// 封装删除
+function removeByValue(arr, val) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == val) {
+            arr.splice(i, 1);
+            break;
+        }
+    }
+}
+// 连接socket
+var io=ws(server);
+io.on("connection",function(socket){
+// 写入成功后读取测试
+fs.readFile('./user.xls', 'utf-8', function (err, data) {
+    if(data!=null){
+    var value = data.split('\n');
+     io.sockets.emit("users", value);	
+    }
+
+});
+// 写入成功后读取测试
+fs.readFile('./password.xls', 'utf-8', function (err,data) {
+    if(data!=null){
+    var pass1=data.split('\n');
+    io.sockets.emit("pass", pass1);
+    }
+});
+    broadcast();
+// 生成名字
+socket.on('setName', function (data) {
+    var name = data;
+    hashName[name] = socket.id;
+    // console.log(hashName[name]);
+    broadcast();
+});
+// 私聊发送
+socket.on('sayTo', function (data) {
+    var toName = data.to;
+    var toId;
+    console.log(toName);
+    if (toId = hashName[toName]) {
+        privateSocket(toId).emit('message1', data);
+    }
+});
+// 离开
+socket.on('disconnect', function (name) {
+         name=this.i2;
+         io.emit("disconnect", name);
+         removeByValue(data, name);
+         io.sockets.emit("dataval", data);
+    })
+// 在线
+socket.on('time', function (val) {
+        // console.log(val);
+   })
+// 注册
+socket.on("reg", function (name) {
+          usocket[name] = socket;
+          this.i1=name;
+          io.emit("reg", name);
+          var myname =this.i1+"\n";
+          fs.writeFile('./user.xls', myname, {
+              'flag': 'a'
+          }, function (err) {
+              if (err) {
+                  throw err;
+              }
+              // 写入成功后读取测试
+              fs.readFile('./user.xls', 'utf-8', function (err,data) {
+                  if (err) {
+                      throw err;
+                  }
+              });
+          });
+    })
+// 加入
+io.emit('connected', ++onlineCount);
+    // console.log(data);
+    io.sockets.emit("array", data);
+    socket.on("join", function (name) {
+        usocket1[name] = socket;
+        this.i2 = name;
+        io.emit("join", name);
+        data.push(name);
+        io.sockets.emit("dataval", data);
+    })
+// 密码
+socket.on("pass",function(val){
+    	pass[val]=socket;
+    	this.i2=val;
+    	io.emit("pass", val);
+    	var password=this.i2+"\n";
+    	 fs.writeFile('./password.xls', password, {
+              'flag': 'a'
+          }, function (err) {
+              if (err) {
+                  throw err;
+              }
+          });
+    })
+});
+server.listen(3003);
+console.log("服务器运行中");
+
+
